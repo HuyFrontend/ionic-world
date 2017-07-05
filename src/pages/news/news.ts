af87a0852c32c0b3cda6f879c1044d8b83d2bbd4
Huy Vo <huyvx@nexlesoft.com>
2017-07-05 10:22:09 +0700

    add action sheet

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,47 @@ import { NavController } from 'ionic-angular';
 })
 export class NewsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  	public navCtrl: NavController,
+  	public platform: Platform,
+    public actionsheetCtrl: ActionSheetController
+  ){}
 
-  }
+  logFunction() {
+  	console.log(123);
+  };
 
+  openMenu() {
+  	let actionSheet = this.actionsheetCtrl.create({
+  		title: 'Albums',
+  		cssClass: 'action-sheets-basic-page',
+  		buttons: [
+	  		{
+	  			text: 'Delete',
+	  			role: 'destructive',
+	  			icon: !this.platform.is('ios') ? 'trash' : null,
+	  			handler: () => {
+	  				console.log('Delete Click');
+	  			}
+	  		},
+	  		{
+	        text: 'Cancel',
+	        role: 'cancel', // will always sort to be on the bottom
+	        icon: !this.platform.is('ios') ? 'close' : null,
+	        handler: () => {
+	          console.log('Cancel clicked');
+	        }
+	      },
+	      {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }
+  		]
+  	});
+
+  	actionSheet.present();
+  };
 }
